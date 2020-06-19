24aac421463b60acc58fa1eb09e4c577af175de8
Balthasar <balthasar.sager@gmail.com>
2020-06-19 19:13:02 +0200

    namespace and imports

--- R/import.R
+++ R/import.R
@@ -1,11 +1,22 @@
+#' @import magrittr
 #' @import dplyr
 #' @import tibble
+#' @import xml2
 #' @import crayon
 #' @import emo
 #' @rawNamespace import(tidyr, except = c(extract))
-#' @import xml2
-#' @importFrom purrr map map_df
+#' @rawNamespace import(memoise, except = c(timeout))
+#' @importFrom httr GET content status_code
+#' @importFrom glue glue
+#' @importFrom assertive assert_is_not_null assert_are_set_equal
+#' @importFrom stringr str_remove_all str_extract
+#' @importFrom readr parse_number
+#' @importFrom purrr map map_df as_vector
 #' @importFrom assertive assert_is_not_null
-#' @import magrittr
+#' @importFrom data.table rbindlist
+#' @importFrom vroom vroom
+#' @importFrom rlang is_empty
+#' @importFrom janitor clean_names
+
 
 NULL
