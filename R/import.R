e936b68db0304f3e949cd1606789f9ad46d6f55d
Balthasar <balthasar.sager@gmail.com>
2021-02-17 22:59:46 +0100

    add newly used packages

--- R/import.R
+++ R/import.R
@@ -4,6 +4,8 @@
 #' @import emo
 #' @rawNamespace import(tidyr, except = c(extract))
 #' @import xml2
+#' @import httr
+#' @import retry
 #' @importFrom purrr map map_df
 #' @importFrom assertive assert_is_not_null
 #' @import magrittr
