dde24f1170654c497e073b6fb76c6d153178b2fc
Balthasar <balthasar.sager@gmail.com>
2021-03-26 13:15:44 +0100

    update imports

--- R/import.R
+++ R/import.R
@@ -2,11 +2,11 @@
 #' @import tibble
 #' @import crayon
 #' @import emo
+#' @importFrom tidyselect everything
 #' @rawNamespace import(tidyr, except = c(extract))
 #' @import xml2
 #' @import httr
-#' @import retry
-#' @importFrom purrr map map_df
+#' @importFrom purrr map map_df map_chr
 #' @importFrom assertive assert_is_not_null
 #' @import magrittr
 
